ebcab9a1cf9404e82b9cfc3364079b0f8d2db31c
shubhamtalpate1007 <shubhamtalpate1007@gmail.com>
2025-11-24 12:37:09 +0530

    Add files via upload

---
+++ helloworld/helloworld/src/main/java/com/example/helloworld/controller/helloController.java
@@ -0,0 +1,18 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+	
+	@GetMapping("/hello")
+	public String helloworld() {
+		
+		return "ok done 👍👍";
+		
+		
+		
+	}
+
+}
